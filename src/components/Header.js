cb48dc8ca47c1806723e4f5431af726cb30a6214
Oluwasanmi Idowu <idowuoluwasanmi@gmail.com>
2022-06-27 11:07:17 +0100

    update

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = ({ handleToggleDarkMode}) => {
+    return (
+        <div className="header">
+            <h1>Notes App</h1>
+            <button className="save"
+                onClick={()=> handleToggleDarkMode((prevDarkMode) => !prevDarkMode)}>
+                    Toggle Mode</button>
+        </div>
+    )
+}
+
+
+export default Header;
